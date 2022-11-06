fc9429e4c3df57a4a1ae54367a47ef3c000fd407
Sofia Golovina <65851895+Zonichka@users.noreply.github.com>
2022-11-06 22:18:25 +0300

    Add files via upload

---
+++ calc_polinoms/cmake-build-debug/CMakeFiles/calc_polinoms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_polinoms.dir/main.cpp.obj"
+  "calc_polinoms.exe"
+  "calc_polinoms.exe.manifest"
+  "calc_polinoms.pdb"
+  "libcalc_polinoms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc_polinoms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
